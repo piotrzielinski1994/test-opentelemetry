5f9a0f4f4ac456983441c05c61f7540e9c8f619a
Piotr Zieliński <piotr.zielinski@sidnet.pl>
2023-02-28 17:00:33 +0100

    Install nodemon

--- videos/videos.js
+++ videos/videos.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Videos');
+  res.send({
+    data: 'Videos',
+  });
 });
 
 app.listen(3000);
