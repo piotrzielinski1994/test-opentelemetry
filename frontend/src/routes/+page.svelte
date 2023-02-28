844281593efff93bf0d0a0e73bed4d57498ec45c
Piotr Zieliński <piotr.zielinski@sidnet.pl>
2023-02-28 17:21:53 +0100

    Prepare base layout for the frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { PUBLIC_DOMAIN } from '$env/static/public';
+  import { onMount } from 'svelte';
+
+  let isLoading = false;
+  let playlists = [];
+  let error = '';
+
+  onMount(() => {
+    isLoading = true;
+
+    fetch(`https://playlists.${PUBLIC_DOMAIN}`)
+      .then((res) => res.json())
+      .then((data) => (playlists = data))
+      .catch((e) => (error = 'Ups, something went wrong'))
+      .finally(() => (isLoading = false));
+  });
+</script>
+
+{#if isLoading}
+  <h2>Loading...</h2>
+{:else if error}
+  <h2 class="error">{error}</h2>
+{:else}
+  {#each playlists as playlist}
+    <h2>{playlist.name}</h2>
+    <ul>
+      {#each playlist.videos as video}
+        <li>{video.name}</li>
+      {/each}
+    </ul>
+  {/each}
+{/if}
+
+<style>
+  :global(body) {
+    min-height: 100vh;
+    display: grid;
+    place-content: center;
+  }
+
+  .error {
+    color: red;
+  }
+</style>
--- playlists/playlists.js
+++ playlists/playlists.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 
-app.get('/', function (req, res) {
-  res.send({
-    data: 'Playlists',
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  const playlists = [
+    {
+      id: 1,
+      name: 'Playlist 1',
+      videos: [
+        {
+          id: 1,
+          name: 'Video 1',
+        },
+        {
+          id: 2,
+          name: 'Video 2',
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Playlist 2',
+      videos: [
+        {
+          id: 3,
+          name: 'Video 3',
+        },
+        {
+          id: 4,
+          name: 'Video 4',
+        },
+      ],
+    },
+  ];
+
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
   });
+
+  res.send(playlists);
 });
 
 app.listen(3000);
