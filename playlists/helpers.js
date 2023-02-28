61e765dfaa105db2b304a57a8e74969f3dce2ff5
Piotr Zieliński <piotr.zielinski@sidnet.pl>
2023-02-28 19:52:33 +0100

    Add counter on the frontend

--- playlists/helpers.js
+++ playlists/helpers.js
@@ -1,9 +1,11 @@
 import { trace } from '@opentelemetry/api';
 import {
-  BasicTracerProvider,
   ConsoleSpanExporter,
   SimpleSpanProcessor,
 } from '@opentelemetry/sdk-trace-base';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
@@ -14,14 +16,18 @@ export const initTracing = (serviceName) => {
   const jaegerExporter = new JaegerExporter({
     endpoint: `http://jaeger:14268/api/traces`,
   });
-
   const provider = new NodeTracerProvider({
     resource: new Resource({
       [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
     }),
   });
+
   provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
   provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+
+  registerInstrumentations({
+    instrumentations: [new ExpressInstrumentation(), new HttpInstrumentation()],
+  });
 
   const tracer = trace.getTracer('tracer-application');
 
--- playlists/playlists.js
+++ playlists/playlists.js
@@ -1,18 +1,21 @@
-import { trace } from '@opentelemetry/api';
+import { trace, propagation, SpanKind, ROOT_CONTEXT } from '@opentelemetry/api';
 import { wait, initTracing } from './helpers.js';
 import express from 'express';
 import cors from 'cors';
 
-const { tracer, provider } = initTracing('my-service');
+const { tracer, provider } = initTracing('asd2');
 trace.setGlobalTracerProvider(provider);
 
 const app = express();
 app.use(cors()).listen(3000);
 
 app.get('/', async (req, res) => {
-  const span = tracer.startSpan('do operation');
-  console.log('@@@ trace | ', trace.getActiveSpan());
-  span.end();
-
+  const remoteCtx = propagation.extract(ROOT_CONTEXT, req.headers);
+  const childSpan = tracer.startSpan('childSpan', remoteCtx);
+  await wait(5000);
+  const childSpan2 = tracer.startSpan('childSpan2', childSpan);
+  await wait(1000);
+  childSpan2.end();
   res.json([]);
+  childSpan.end();
 });
