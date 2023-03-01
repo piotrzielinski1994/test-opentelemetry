3e607314c7cc3840395ad06beda75247ed791df9
Piotr Zieliński <piotr.zielinski@sidnet.pl>
2023-03-01 01:45:54 +0100

    Remove console.log from playlists-api.js

--- playlists/playlists-api.js
+++ playlists/playlists-api.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import initTracing from './playlists-tracing.js';
 import playlistsDb from './playlists-db.js';
 import { videosClient } from './playlists-clients.js';
+import { SpanStatusCode } from '@opentelemetry/api';
 
 const { getContextFromRequest, startSpan } = initTracing('playlists-ms');
 
@@ -24,8 +25,13 @@ app.get('/playlists', async (req, res) => {
 
     for (const videoId of playlist.videoIds) {
       const [videoSpan] = startSpan(`videos-ms | Get ${videoId}`, wrapperContext);
-      const video = await videosClient.getVideo(videoId);
-      // console.log('@@@ video | ', video);
+      const video = await videosClient.getVideo(videoId).catch((e) => {
+        videoSpan.setStatus({
+          code: SpanStatusCode.ERROR,
+          message: e.message,
+        });
+        throw e;
+      });
       videos.push(video);
       videoSpan.end();
     }
